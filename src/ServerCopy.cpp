9425fa07e5b6fa3fa81e6d0d07d96caf0a4dceb2
jeza <jezaguerin.pro@gmail.com>
2024-10-22 20:38:43 +0200

    Implementing css & html page & je kiffe ça

--- src/ServerCopy.cpp
+++ src/ServerCopy.cpp
@@ -325,102 +325,102 @@ void Server::serveStaticFile(int client_fd, const std::string& filePath) {
 
 
 
-// void Server::stockClientsSockets(Socket& socket) {
-// 	int add_size = sizeof(socket.getAddress());
+void Server::stockClientsSockets(Socket& socket) {
+	int add_size = sizeof(socket.getAddress());
 
-// 	int client_fd = accept(socket.getSocket(), (struct sockaddr *)&socket.getAddress(), (socklen_t*)&add_size);
-// 	if (client_fd == -1) {
-// 		std::cerr << "Failed to accept an incoming connection. errno: " << strerror(errno) << std::endl;
-// 		return;
-// 	}
+	int client_fd = accept(socket.getSocket(), (struct sockaddr *)&socket.getAddress(), (socklen_t*)&add_size);
+	if (client_fd == -1) {
+		std::cerr << "Failed to accept an incoming connection. errno: " << strerror(errno) << std::endl;
+		return;
+	}
 
-// 	std::cout << "Connection accepted with client_fd: " << client_fd << std::endl;
+	std::cout << "Connection accepted with client_fd: " << client_fd << std::endl;
 
-// 	if (client_fd >= 0) {
-// 		_clientsFd.push_back(client_fd);
-// 	} else {
-// 		std::cerr << "Invalid client file descriptor: " << client_fd << std::endl;
-// 	}
-// }
-
-
-void    Server::stockClientsSockets(Socket& socket) {
-    pollfd  server_pollfd;
-
-    server_pollfd.fd = socket.getSocket();
-    server_pollfd.events = POLLIN; // Surveiller les connexions entrantes. (POLLIN : lecture)
-    fds.push_back(server_pollfd);
-
-    while (true) {
-        int fds_nb = poll(fds.data(), fds.size(), -1);
-        if (fds_nb == -1) {
-            std::cout << "Error while checking client requests" << std::endl;
-            return ;
-        }
-        for (std::vector<pollfd>::iterator it = fds.begin(); it != fds.end();) {
-            // Need to manage POLLERR - POLLHUP - POLLNVAL => Check if we use strerror(errno) for error log.
-            // if (it->revents & POLLERR) { // signifie qu'une erreur non récupérable s'est produite.
-            //     std::cout << "An error occured on this socket !" << std::endl;
-            //     close (it->fd);
-            //     it = fds.erase(it);
-            //     break ;
-            // }
-            // else if (it->revents & POLLHUP) { // indique que le client a fermé la connexion proprement.
-            //     std::cout << "Connection has been closed by client." << std::endl;
-            //     close (it->fd);
-            //     it = fds.erase(it);
-            //     break ;
-            // }
-            // else if (it->revents & POLLNVAL) { // fd est invalide (ex., si tu utilises un socket fermé ou non valide dans poll())
-            //     std::cout << "File descriptor is not valid." << std::endl;
-            //     close(it->fd);
-            //     it = fds.erase(it);
-            //     break ;
-            // }
-            if (it->revents & POLLIN) { // Si le fd est prêt à être lu. > & : vérifie si le bit correspondant à POLLIN est activé dans revents
-                if (it->fd == socket.getSocket()) { // Si c'est le socket serveur > Accept new connection.
-					int add_size = sizeof(socket.getAddress());
-                    int client_socket = accept(socket.getSocket(), (struct sockaddr*)&socket.getAddress(), (socklen_t*)&add_size);
-                    if (client_socket == -1) {
-                        std::cout << "Failed to accept client connection." << std::endl;
-                        continue ;
-                    }
-                    pollfd  client_pollfd;
-
-                    client_pollfd.fd = client_socket;
-                    client_pollfd.events = POLLIN;
-                    fds.push_back(client_pollfd);
-                    std::cout << "New client connected." << std::endl;
-                }
-                // Means that the event is linked to a client request.
-                else {
-                    char    buffer[1024] = {0};
-                    int bytes_rcv = recv(it->fd, buffer, 1024, 0);
-
-                    if (bytes_rcv == 0) {
-                        close (it->fd);
-                        it = fds.erase(it); // Après avoir appelé erase(it), l'iterator est invalidé, donc il faut mettre à jour l'iterator pour qu'il pointe sur le bon élément suivant
-                        std::cout << "Connection has been closed by the client." << std::endl;
-                        continue ;
-                    }
-                    else if (bytes_rcv == -1) {
-                        close (it->fd);
-                        it = fds.erase(it); // Après avoir appelé erase(it), l'iterator est invalidé, donc il faut mettre à jour l'iterator pour qu'il pointe sur le bon élément suivant
-                        std::cout << "Failed in receiving client request." << std::endl;
-                        continue ;
-                    }
-
-                    std::cout << "Client request is : " << buffer << std::endl;
-                    std::string response = "This is a fucking response waiting by another client !";
-
-                    int bytes_sent = send(it->fd, response.c_str(), response.size(), 0);
-                    if (bytes_sent == -1) {
-                        std::cout << "Failed in sending response to client." << std::endl;
-                        continue ;
-                    }
-                }
-            }
-			++it;
-        }
-    }
+	if (client_fd >= 0) {
+		_clientsFd.push_back(client_fd);
+	} else {
+		std::cerr << "Invalid client file descriptor: " << client_fd << std::endl;
+	}
 }
+
+
+// void    Server::stockClientsSockets(Socket& socket) {
+//     pollfd  server_pollfd;
+
+//     server_pollfd.fd = socket.getSocket();
+//     server_pollfd.events = POLLIN; // Surveiller les connexions entrantes. (POLLIN : lecture)
+//     fds.push_back(server_pollfd);
+
+//     while (true) {
+//         int fds_nb = poll(fds.data(), fds.size(), -1);
+//         if (fds_nb == -1) {
+//             std::cout << "Error while checking client requests" << std::endl;
+//             return ;
+//         }
+//         for (std::vector<pollfd>::iterator it = fds.begin(); it != fds.end();) {
+//             // Need to manage POLLERR - POLLHUP - POLLNVAL => Check if we use strerror(errno) for error log.
+//             // if (it->revents & POLLERR) { // signifie qu'une erreur non récupérable s'est produite.
+//             //     std::cout << "An error occured on this socket !" << std::endl;
+//             //     close (it->fd);
+//             //     it = fds.erase(it);
+//             //     break ;
+//             // }
+//             // else if (it->revents & POLLHUP) { // indique que le client a fermé la connexion proprement.
+//             //     std::cout << "Connection has been closed by client." << std::endl;
+//             //     close (it->fd);
+//             //     it = fds.erase(it);
+//             //     break ;
+//             // }
+//             // else if (it->revents & POLLNVAL) { // fd est invalide (ex., si tu utilises un socket fermé ou non valide dans poll())
+//             //     std::cout << "File descriptor is not valid." << std::endl;
+//             //     close(it->fd);
+//             //     it = fds.erase(it);
+//             //     break ;
+//             // }
+//             if (it->revents & POLLIN) { // Si le fd est prêt à être lu. > & : vérifie si le bit correspondant à POLLIN est activé dans revents
+//                 if (it->fd == socket.getSocket()) { // Si c'est le socket serveur > Accept new connection.
+// 					int add_size = sizeof(socket.getAddress());
+//                     int client_socket = accept(socket.getSocket(), (struct sockaddr*)&socket.getAddress(), (socklen_t*)&add_size);
+//                     if (client_socket == -1) {
+//                         std::cout << "Failed to accept client connection." << std::endl;
+//                         continue ;
+//                     }
+//                     pollfd  client_pollfd;
+
+//                     client_pollfd.fd = client_socket;
+//                     client_pollfd.events = POLLIN;
+//                     fds.push_back(client_pollfd);
+//                     std::cout << "New client connected." << std::endl;
+//                 }
+//                 // Means that the event is linked to a client request.
+//                 else {
+//                     char    buffer[1024] = {0};
+//                     int bytes_rcv = recv(it->fd, buffer, 1024, 0);
+
+//                     if (bytes_rcv == 0) {
+//                         close (it->fd);
+//                         it = fds.erase(it); // Après avoir appelé erase(it), l'iterator est invalidé, donc il faut mettre à jour l'iterator pour qu'il pointe sur le bon élément suivant
+//                         std::cout << "Connection has been closed by the client." << std::endl;
+//                         continue ;
+//                     }
+//                     else if (bytes_rcv == -1) {
+//                         close (it->fd);
+//                         it = fds.erase(it); // Après avoir appelé erase(it), l'iterator est invalidé, donc il faut mettre à jour l'iterator pour qu'il pointe sur le bon élément suivant
+//                         std::cout << "Failed in receiving client request." << std::endl;
+//                         continue ;
+//                     }
+
+//                     std::cout << "Client request is : " << buffer << std::endl;
+//                     std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 60\r\n\r\nThis is a fucking response waiting by another client !";
+
+//                     int bytes_sent = send(it->fd, response.c_str(), response.size(), 0);
+//                     if (bytes_sent == -1) {
+//                         std::cout << "Failed in sending response to client." << std::endl;
+//                         continue ;
+//                     }
+//                 }
+//             }
+// 			++it;
+//         }
+//     }
+// }
