97756d7b7377ff918c778cfeef5288a0a7413d81
Tulece <antoine.porceddu@hotmail.ca>
2024-11-08 13:24:59 +0100

    deleted absolute path from config LOL + moved index.html from pages to www

--- www/cgi-bin/contact.php
+++ www/cgi-bin/contact.php
@@ -1,35 +1,3 @@
-<!--php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-echo "<html><body><h1>Merci pour votre message!</h1>";
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Récupérer et sécuriser les données du formulaire
-    $nom = htmlspecialchars($_POST['nom']);
-    $prenom = htmlspecialchars($_POST['prénom']);
-    $email = htmlspecialchars($_POST['email']);
-    $tel = htmlspecialchars($_POST['tel']);
-    $sujet = htmlspecialchars($_POST['sujet']);
-    $message = htmlspecialchars($_POST['message']);
-
-    // Afficher les données reçues
-    echo "<p><strong>Nom :</strong> $nom</p>";
-    echo "<p><strong>Prénom :</strong> $prenom</p>";
-    echo "<p><strong>Email :</strong> $email</p>";
-    echo "<p><strong>Téléphone :</strong> $tel</p>";
-    echo "<p><strong>Sujet :</strong> $sujet</p>";
-    echo "<p><strong>Message :</strong><br>" . nl2br($message) . "</p>";
-} else {
-    // Si la requête n'est pas en POST, afficher un message ou rediriger
-    echo "<p>Veuillez soumettre le formulaire.</p>";
-}
-
-echo "</body></html>";
- -->
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -73,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Destinataires
         $mail->setFrom($email, "$nom $prenom"); // L'email de l'expéditeur (l'utilisateur qui remplit le formulaire)
-        $mail->addAddress('jeguerin@student.42perpignan.fr', 'Teddy Gibert'); // Votre adresse email où vous recevrez le message
+        $mail->addAddress('anporced@student.42perpignan.fr', 'Teddy Gibert'); // Votre adresse email où vous recevrez le message
 
         // Contenu de l'email
         $mail->isHTML(false); // Paramétrer sur true si vous envoyez un email au format HTML
