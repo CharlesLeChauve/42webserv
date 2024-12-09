9d2160ef3e37cc0d08afcbf3f989e086a38c62cf
Teddy GIBERT <teddygibert@Teddys-MacBook-Air.local>
2024-12-09 16:47:33 +0100

    test scripts added on index page, quitty not rovoking the attented behavior

---
+++ www/cgi-bin/quitty.php
@@ -0,0 +1,4 @@
+<?php
+//This scripts just exit, without writing anything in output
+exit(1);
+?>
---
+++ www/cgi-bin/sleepy.php
@@ -0,0 +1,5 @@
+<?php
+//This scripts sleeps 30 seconds before doing anything, Server will consider timeout and kill this proces
+sleep(30);
+echo "Hello world ! I just slept 30 seconds.";
+?>
